5dd159ddd4e373397ab478784d3f81b73019a4a1
Michael Gaare <michael.gaare@gmail.com>
2018-09-22 02:07:02 -0400

    Rewrite the encoding functions, 19x faster now

--- src/com/michaelgaare/clojure_polyline.clj
+++ src/com/michaelgaare/clojure_polyline.clj
@@ -39,11 +39,11 @@
 (defn coords->vec [coords]
   (map (fn [{:keys [latitude longitude]}] [latitude longitude]) coords))
 
-(defn- coord->int [coord]
+(defn- coord->int
+  [coord]
   (-> coord
       (* 1e5)
-      Math/rint
-      int))
+      Math/round))
 
 (defn ints->str [ints]
   (->> ints
@@ -75,48 +75,49 @@
     (bit-not int)
     int))
 
-(defn- partition-bits
-  "takes an integer, and the number of bits in segments, and returns a seq of
-   integers which correspond to the integer broken down into segments with the
-   given number of bits"
-  [n bits]
-  (when (> n 0)
-    (let [bits-int (reduce * (repeat bits 2))
-          cur (mod n bits-int)
-          rem (/ (- n cur) bits-int)]
-      (cons cur (partition-bits rem bits)))))
-
-(defn- pad-ints [ints]
-  (let [end (last ints)]
-    (->> ints
-         drop-last
-         (map (partial + 32))
-         reverse
-         (cons end)
-         (map (partial + 63))
-         reverse)))
-
-(defn encode-coord [coord]
+(defn coord->integer
+  "Returns the coord (a double) multiplied by 100,000 and rounded to
+   nearest integer."
+  ^long [coord]
   (-> coord
-      coord->int
-      (bit-shift-left 1)
-      invert-negative
-      (partition-bits 5)
-      pad-ints
-      ints->str))
+      (* 1e5)
+      Math/round))
 
-(defn compact-coords 
+(defn encode-coord
+  "Returns the polyline encoded string of given coordinate (a double
+   between -180 and 180)."
+  [x]
+  (let [coord-int (-> x
+                      coord->integer
+                      (bit-shift-left 1)
+                      invert-negative
+                      int)]
+    (loop [rem coord-int
+           sb (StringBuilder.)]
+      (let [nxt-rem (bit-shift-right rem 5)
+            chunk (cond-> (bit-and rem 31)
+                    (pos? nxt-rem) (bit-or 32))
+            sb (.append sb (char (+ chunk 63)))]
+        (if (pos? nxt-rem)
+          (recur nxt-rem sb)
+          (.toString sb))))))
+
+(defn compact-coords
   "Takes a vector of coord vectors, and returns a vector of vectors of
    the difference from the previous coord. The format that polyline wants"
-  ([coords]
-     (let [c (reverse coords)]
-       (when-let [x (first c)]
-         (compact-coords x (rest c)))))
-  ([x rem]
-     (if-let [y (first rem)]
-       (conj (compact-coords y (rest rem)) (vec (map - x y)))
-       [x])))
+    ([coords]
+     (when-let [x (first coords)]
+       (cons x (compact-coords x (rest coords)))))
+    ([x coords]
+     (when-let [y (first coords)]
+       (let [[xlat xlong] x
+             [ylat ylong] y]
+         (cons [(- ylat xlat) (- ylong xlong)] (compact-coords y (rest coords)))))))
 
-(defn encode [coords] "Main encoding interface"
-  (let [c (flatten (compact-coords coords))]
-    (apply str (map encode-coord c))))
+(defn encode
+  "Main polyline encoding function. Takes a collection of [lat long]
+   tuples and returns the polyline encoded string representation."
+  [coords]
+  (transduce (comp cat (map encode-coord))
+             str
+             (compact-coords coords)))
