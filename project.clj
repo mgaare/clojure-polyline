4fbb1e034b4de5914de66c27ac354fb25becfe27
Alexander Scott <xander@cadcoder.com>
2018-11-07 10:45:38 +1000

    Use parent

--- project.clj
+++ project.clj
@@ -3,18 +3,19 @@
 (defproject io.jesi/clojure-polyline VERSION
   :description "library to encode and decode google polyline algorithm"
   :url "http://github.com/jesims/clojure-polyline"
-  :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.10.339" :exclusions [com.google.javascript/closure-compiler-unshaded]]
+  :license {:name         "Eclipse Public License"
+            :url          "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments     "same as Clojure"}
+  :plugins [[lein-parent "0.3.5"]]
+  :parent-project {:coords  [io.jesi/parent "0.0.2"]
+                   :inherit [:plugins :managed-dependencies :deploy-repositories :dependencies :exclusions [:profiles :dev]]}
+  :dependencies [[org.clojure/clojure]]
+  :profiles {:dev {:dependencies [[org.clojure/clojurescript]
                                   [org.clojure/tools.namespace "0.2.11"]
-                                  [criterium "0.4.4"]
-                                  [thheller/shadow-cljs "2.6.22"]]
+                                  [thheller/shadow-cljs]]
                    :plugins      [[lein-shell "0.5.0"]]
                    :source-paths ["dev"]}}
-  :aliases {"node-test" ["do" "run" "-m" "shadow.cljs.devtools.cli" "release" "test," "shell" "node" "target/node/test.js"]}
-  :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
-                                    :username      :env/clojars_username
-                                    :password      :env/clojars_password
-                                    :sign-releases false}]])
-
+  :aliases {"node-test" ["do"
+                         "run" "-m" "shadow.cljs.devtools.cli" "release" "test,"
+                         "shell" "node" "target/node/test.js"]})
